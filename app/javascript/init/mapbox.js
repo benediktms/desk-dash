b2b575f3709c4c525985aab0792690ceb49fffd8
Benedikt Schnatterbeck <benedikt.schnatterbeck@gmail.com>
2020-08-08 15:32:49 +0100

    added more padding to force map to load more data, to stop it from lazy loading only partially

--- app/javascript/init/mapbox.js
+++ app/javascript/init/mapbox.js
@@ -34,5 +34,5 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.longitude, marker.latitude ]));
-  map.fitBounds(bounds, { padding: 50, maxZoom: 15, duration: 1000 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 1000 });
 };
